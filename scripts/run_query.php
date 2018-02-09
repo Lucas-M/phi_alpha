16e3cbb916725e1f51da91db18d8cb37c08fa667
Luke <lucas.macd@gmail.com>
2018-02-09 12:56:15 -0500

    Updated fetch_assoc to fetch_array. Now the query executor is functional.

--- scripts/run_query.php
+++ scripts/run_query.php
@@ -13,14 +13,38 @@ if ($conn->connect_error) {
 $query_text = $_REQUEST['query'];
 $result = $conn->query($query_text);
 
+$return_rows = false;
+$location = strpos($query_text, "CREATE");
+if ($location === false) {
+  $location = strpos($query_text, "INSERT");
+  if ($location === false) {
+    $location = strpos($query_text, "UPDATE");
+      if ($location === false) {
+	    $location = strpos($query_text, "DELETE");
+	      if ($location === false) {
+	      $location = strpos($query_text, "DROP");
+    	    if ($location === false) {
+    	    // If we got here, it's not a create, insert, update, delete or 
+    	    // drop query. It should return rows.
+    	      $return_rows = true;
+		    }
+	      }
+	  }
+   }
+}
+
+
+
 echo "<p>Result from your query:</p>";
 echo "<ul>";
 
 if ($result->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
+    // while($row = $result->fetch_assoc()) { //this version was wrong...assoc did not work
+    while($row = $result->fetch_array()) {
         // echo "<li>{$row[0]}</li>";
-        echo "<li>" . $row["Tables_in_fruits"] . "</li>";
+        // echo "<li>" . $row["Tables_in_fruits"] . "</li>";
+        echo "<li>" . $row[0] . " " . $row[1] . " " . $row[2] . "</li>";
     }
 } else {
     echo "<li>0 results</li>";
